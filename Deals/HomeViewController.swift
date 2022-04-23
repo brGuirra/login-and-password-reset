d0f3465ac09d69c9728f770ae06f9e4a8afd6b6a
brGuirra <br.guirra@gmail.com>
2022-04-23 16:55:51 -0300

    initial commit

---
+++ Deals/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Deals
+//
+//  Created by Bruno Guirra on 23/04/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemCyan
+    }
+}
+
